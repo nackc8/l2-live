26730b44e9a9c9d28d7a2ff75d67af53f805929d
Kent Larsson <kent@c8.se>
2025-01-09 13:04:27 +0100

    autocommit complete 1304 (27)

--- date/d250109/ex3/process.sh
+++ date/d250109/ex3/process.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-src=original
-dst=modified
+src="$1"
+dst="$2"
 
 while true; do
     for src_img in "$src"/*; do
-        sleep 3
         dst_img="$dst/$(basename "$src_img")"
         if [ ! -e "$dst_img" ]; then
-            echo "Skapar $dst_img"
-            touch "$dst_img"
+            echo "$(date) skapar $dst_img"
+            convert "$src_img" -colorspace Gray "$dst_img"
         fi
     done
-    sleep 10
+    sleep 5
 done
