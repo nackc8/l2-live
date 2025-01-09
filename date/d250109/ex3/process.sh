7dd57b77346cea74447881702174f38fa8341f9a
Kent Larsson <kent@c8.se>
2025-01-09 12:59:13 +0100

    autocommit complete 1259 (13)

--- date/d250109/ex3/process.sh
+++ date/d250109/ex3/process.sh
@@ -5,8 +5,12 @@ dst=modified
 
 while true; do
     for src_img in "$src"/*; do
+        sleep 3
         dst_img="$dst/$(basename "$src_img")"
-        echo "$dst_img"
+        if [ ! -e "$dst_img" ]; then
+            echo "Skapar $dst_img"
+            touch "$dst_img"
+        fi
     done
     sleep 10
 done
