7eda759ba69104fd4ec8c4762b9b5989d3f40a68
Kent Larsson <kent@c8.se>
2025-01-09 12:54:50 +0100

    autocommit complete 1254 (50)

--- date/d250109/ex3/process.sh
+++ date/d250109/ex3/process.sh
@@ -5,7 +5,7 @@ dst=modified
 
 while true; do
     for src_img in "$src"/*; do
-        dst_img="$(basename "$src_img")"
+        dst_img="$dst/$(basename "$src_img")"
         echo "$dst_img"
     done
     sleep 10
